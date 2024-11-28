26bc698bec9400ff0b008dcdc80c23f475b0bdbb
semihtahirakbas <tahir.akbas@hotmail.com>
2024-11-28 18:07:11 +0300

    Adding-network-creating-viewmodels

---
+++ MovieApp/Models/MovieModel.swift
@@ -0,0 +1,11 @@
+//
+//  FilmModel.swift
+//  MovieApp
+//
+//  Created by Tahir Akbas on 28.11.2024.
+//
+
+struct FilmModel: Encodable{
+    let _id: String
+    let 
+}
---
+++ MovieApp/Views/HomeView/HomeRowFilmView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeRowFilmView.swift
+//  MovieApp
+//
+//  Created by Tahir Akbas on 28.11.2024.
+//
+
+import SwiftUI
+
+struct HomeRowFilmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeRowFilmView()
+}
