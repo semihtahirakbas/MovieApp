8489701c0ff1dd89bef439804b1990174dac54c2
semihtahirakbas <tahir.akbas@hotmail.com>
2024-12-04 18:30:49 +0300

    adding-search-page

---
+++ MovieApp/Views/MovieView/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetail.swift
+//  MovieApp
+//
+//  Created by Tahir Akbas on 4.12.2024.
+//
+import SwiftUI
+
+struct MovieDetailView: View{
+    
+    var body: some View{
+        Text("Hello")
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
